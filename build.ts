6c825b3cb1f02d1299e410ef5251e89b06d62080
radian628 <70352880+radian628@users.noreply.github.com>
2025-10-22 11:44:54 -0700

    create automated build process

--- build.ts
+++ build.ts
@@ -1,6 +1,8 @@
 import { smartAsyncReplaceAll } from "./r628/src/stringutils";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
+import * as chokidar from "chokidar";
+import * as sassCompiler from "sass";
 
 async function smartStringReplace(str: string) {
   return (
@@ -11,9 +13,12 @@ async function smartStringReplace(str: string) {
   ).str;
 }
 
-// @ts-expect-error
-globalThis.file = async (str, enclosingTag) => {
-  const ret = await smartStringReplace(
+async function generalizedFileImport(
+  transformer: (str: string) => Promise<string>,
+  str: string,
+  enclosingTag: string
+) {
+  const ret = await transformer(
     (await fs.readFile(path.join("src", str))).toString()
   );
 
@@ -21,8 +26,28 @@ globalThis.file = async (str, enclosingTag) => {
     return `<${enclosingTag}>${ret}</${enclosingTag}>`;
   }
   return ret;
+}
+
+// @ts-expect-error
+globalThis.file = async (str, enclosingTag) => {
+  return generalizedFileImport(smartStringReplace, str, enclosingTag);
 };
 
-// const page = (await fs.readFile("src/page.txt")).toString();
+// @ts-expect-error
+globalThis.sass = async (str, enclosingTag) => {
+  return generalizedFileImport(
+    async (s) => (await sassCompiler.compileStringAsync(s)).css,
+    str,
+    enclosingTag
+  );
+};
 
-// fs.writeFile("static/source.txt", await smartStringReplace(page));
+chokidar
+  .watch("src", {
+    usePolling: true,
+    ignoreInitial: false,
+  })
+  .on("all", async () => {
+    const page = (await fs.readFile("src/page.txt")).toString();
+    fs.writeFile("static/source.txt", await smartStringReplace(page));
+  });
