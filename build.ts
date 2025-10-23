f29078fa3f7b385f40a889cd099b41d6e7c08725
radian628 <70352880+radian628@users.noreply.github.com>
2025-10-22 20:45:14 -0700

    improved build script

--- build.ts
+++ build.ts
@@ -3,6 +3,23 @@ import * as fs from "node:fs/promises";
 import * as path from "node:path";
 import * as chokidar from "chokidar";
 import * as sassCompiler from "sass";
+import { exec } from "node:child_process";
+import { stdout, stderr } from "node:process";
+
+const devopsServer = exec("wds run");
+
+function indent(str: string, indentWith: string) {
+  return indentWith + str.replaceAll("\n", "\n" + indentWith);
+}
+
+devopsServer.stderr?.on("data", (d) => {
+  stderr.write("Server:\n");
+  stderr.write(indent(d.trimEnd(), "| ") + "\n\n");
+});
+devopsServer.stdout?.on("data", (d) => {
+  stdout.write("Server:\n");
+  stdout.write(indent(d.trimEnd(), "| ") + "\n\n");
+});
 
 async function smartStringReplace(str: string) {
   return (
@@ -42,12 +59,31 @@ globalThis.sass = async (str, enclosingTag) => {
   );
 };
 
+// @ts-expect-error
+globalThis.writeFile = async (filename: string, str: string) => {
+  await fs.writeFile(filename, str);
+  return "";
+};
+
+function log(str: string) {
+  stdout.write("Build:\n");
+  stdout.write(indent(str, "| ") + "\n\n");
+}
+
+async function rebuild() {
+  log("Started build!");
+  const page = (await fs.readFile("src/page.txt")).toString();
+  fs.writeFile("static/source.txt", await smartStringReplace(page));
+  log("Finished build!");
+}
+
 chokidar
   .watch("src", {
     usePolling: true,
-    ignoreInitial: false,
+    ignoreInitial: true,
   })
   .on("all", async () => {
-    const page = (await fs.readFile("src/page.txt")).toString();
-    fs.writeFile("static/source.txt", await smartStringReplace(page));
+    rebuild();
   });
+
+rebuild();
